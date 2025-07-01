70478413b9c8b3b15d19a727c219cc530f1fe0a8
3or1Ryoga <sakanaliang@gmail.com>
2025-07-01 23:27:55 +0900

    abndsfnk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  redirect('https://ryoga-xi.vercel.app/contact')
+  redirect('https://ryoga-xi.vercel.app/')
 
   // redirectがサーバー側で実行されるため、この下のコードは実際には実行されません。
   // return null; bkfjdasbdfkdas
