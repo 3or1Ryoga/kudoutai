c708eb9d038168f239a6e6a51c06b00ae84cb818
3or1Ryoga <sakanaliang@gmail.com>
2025-07-01 23:17:56 +0900

    fuck

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   redirect('https://ryoga-xi.vercel.app/contact')
 
   // redirectがサーバー側で実行されるため、この下のコードは実際には実行されません。
-  // return null;
+  // return null; bkfjdasbdfkdas
 }
 
 //  bafudibiubaknajdsnfabfjkasndaskn
